518f3c65fc2c7ff3c8a3d283d40d0b4c358ba853
nasim maleki <maleki.nasim@gmail.com>
2025-02-25 13:36:28 +0100

    change factory method

--- DesignPatern/flyweightPattern/src/icon/factory/IconFactory.java
+++ DesignPatern/flyweightPattern/src/icon/factory/IconFactory.java
@@ -8,22 +8,15 @@ import icon.iconimpl.FolderIcon;
 import java.util.HashMap;
 import java.util.Map;
 
-import static icon.CustomIconType.FILE_ICON;
-import static icon.CustomIconType.FOLDER_ICON;
 
 public class IconFactory {
   private static final Map<CustomIconType, Icon> shapeMap=new HashMap<>();
 
   public static Icon getIcon(CustomIconType type){
-      return shapeMap.computeIfAbsent(type,key-> {
-                  switch (key) {
-                      case FOLDER_ICON:
-                          return new FolderIcon();
-                      case FILE_ICON:
-                          return new FileIcon();
-                      default: throw new IllegalArgumentException("Unknown icon type");
-                  }
-              }
+      return shapeMap.computeIfAbsent(type,key-> switch (key) {
+          case FOLDER_ICON -> new FolderIcon();
+          case FILE_ICON -> new FileIcon();
+      }
       );
       }
 }
