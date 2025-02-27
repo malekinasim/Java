5d5008e9e3e2f1c87f275e81dd8928477e40e14b
nasim maleki <maleki.nasim@gmail.com>
2025-02-27 17:48:39 +0100

    combine builder by other pattern

--- DesignPatern/builderPattern/src/main/java/org/example/CombiningBuilderByFlyweight/ButtonFactory.java
+++ DesignPatern/builderPattern/src/main/java/org/example/CombiningBuilderByFlyweight/ButtonFactory.java
@@ -4,14 +4,14 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public class ButtonFactory {
-    private static Map<String, Button> buttons = new WeakHashMap<>();
+    private static final Map<String, Button> buttons = new WeakHashMap<>();
 
     public static Button getButton(Button.ButtonType buttonType, String text,
                                    Runnable action, ButtonStyle buttonStyle) {
         if (buttonType == null || text == null || buttonStyle == null) {
             throw new IllegalArgumentException("ButtonType, text, and ButtonStyle cannot be null.");
         }
-        String k = buttonType.name() + "-" + text + "-" + buttonStyle.hashCode();
+        String k = String.format("%s-%s-%s", buttonType.name() ,text , buttonStyle.hashCode());
         return buttons.computeIfAbsent(k, (key) ->
                 (new Button.ButtonBuilder(text, buttonType))
                         .setAction(action)
@@ -24,8 +24,9 @@ public class ButtonFactory {
         if (buttonType == null || text == null ) {
             throw new IllegalArgumentException("ButtonType and text cannot be null.");
         }
+
         ButtonStyle buttonStyle= ButtonStyleFactory.getStyle(backgroundColor, textColor, borderRadius);
-        String k = buttonType.name() + "-" + text + "-" + buttonStyle.hashCode();
+        String k = String.format("%s-%s-%s", buttonType.name() ,text , buttonStyle.hashCode());
        return   getButton(buttonType, text, action, buttonStyle);
     }
 }
