43e587268009d182001c38128124d1403fff47e2
maleki.nasim <maleki.nasim@gmail.com>
2025-08-09 13:03:35 +0200

    move class

--- employee.task.tracker/src/main/java/com/example/employee/task/tracker/config/FilterConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.employee.task.tracker.config;
-public class FilterConstants {
-    public static final String STATUS_FILTER = "statusFilter";
-    public static final String STATUS_FILTER_PARAM= "status_prm";
-
-
-    public static final String TENANT_FILTER = "tenantFilter";
-    public static final String TENANT_FILTER_PARAM = "tenant_prm";
-}
---
+++ employee.task.tracker/src/main/java/com/example/employee/task/tracker/config/hibernate/FilterConstants.java
@@ -0,0 +1,9 @@
+package com.example.employee.task.tracker.config.hibernate;
+public class FilterConstants {
+    public static final String STATUS_FILTER = "statusFilter";
+    public static final String STATUS_FILTER_PARAM= "status_prm";
+
+
+    public static final String ORGAN_FILTER = "organFilter";
+    public static final String ORGAN_FILTER_PARAM = "organ_prm";
+}
